f55308deb906725223abe48449c13df05694343a
medericmesle <mederic.mesle@gmail.com>
2022-01-30 15:00:14 +0100

    added 2 validations to booking model

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,4 +2,7 @@ class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :candidate
   has_many :reviews
+
+  validates :start_date, date: { before: :end_date }
+  validates :total_price, numericality: { only_integer: true }
 end
