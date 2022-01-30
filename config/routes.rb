efcb7eb09550c6eb7b79cd8accfc84922a2820b4
christophe126 <christophe126@gmail.com>
2022-01-30 18:57:58 +0100

    routes added

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+  resources :candidates, only: %i[index show new create update destroy] do
+    resources :booking, only: [:new]
+  end
+  resources :booking, only: %i[index update edit]
+  resources :reviews, only: %i[new destroy]
 end
