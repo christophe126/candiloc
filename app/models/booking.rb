7d3442c3f63eaba2efda11a36ac4f57cf6b945a3
Bastien <genin440@gmail.com>
2022-02-03 19:41:49 +0100

    Coded things in _new_booking.html.erb

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,6 +3,10 @@ class Booking < ApplicationRecord
   belongs_to :candidate
   has_many :reviews
 
+  validates :start_date, presence: true
+
+  validates :end_date, presence: true
+
   validates :start_date, format: { with: %r{[1-9]{1}[0-9]{1}/[0-9]{2}/[0-9]{4}} }
 
   validates :start_date, date: { before: :end_date }
