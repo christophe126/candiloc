033938f501d75eb79d37c46600489f6d6d88f67a
Bastien Genin <genin440@gmail.com>
2022-02-04 20:45:48 +0100

    Work in progress on the booking contorller

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -7,9 +7,9 @@ class Booking < ApplicationRecord
 
   validates :end_date, presence: true
 
-  validates :start_date, format: { with: %r{[1-9]{1}[0-9]{1}/[0-9]{2}/[0-9]{4}} }
+  # validates :start_date, format: { with: %r{[1-9]{1}[0-9]{1}/[0-9]{2}/[0-9]{4}} }
 
-  validates :start_date, date: { before: :end_date }
+  # validates :start_date, date: { before: :end_date }
 
   validates :total_price, numericality: { only_integer: true }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :candidates, only: %i[index show new create update destroy] do
-    resources :bookings, only: [:new]
+    resources :bookings, only: [:new, :create]
   end
   resources :bookings, only: %i[index update edit]
   resources :reviews, only: %i[new destroy]
