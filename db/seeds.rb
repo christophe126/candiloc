bf342f5c6819a0641b88ecc20d25ae6be736a334
medericmesle <mederic.mesle@gmail.com>
2022-02-05 13:58:15 +0100

    seed content updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,9 +8,13 @@ puts 'Let the madness begin'
 
 puts '----------Creating political parties---------'
 political_one = PoliticalParty.create(name: 'En Marche')
-political_two = PoliticalParty.create(name: 'Les Républicains')
+political_two = PoliticalParty.create(name: 'Les Insoumis')
 political_three = PoliticalParty.create(name: 'Indépendant')
 political_four = PoliticalParty.create(name: 'Nouveau parti anticapitaliste')
+political_five = PoliticalParty.create(name: 'Libres !')
+political_six = PoliticalParty.create(name: 'Europe Ecologie-Les Verts')
+political_seven = PoliticalParty.create(name: 'Parti animaliste')
+political_eight = PoliticalParty.create(name: 'Union populaire républicaine')
 puts '----------Creating political parties done---------'
 
 puts '----------Creating new users---------'
@@ -49,7 +53,7 @@ candidate_one.political_party = political_one
 candidate_one.user = user_admin
 candidate_one.save
 
-puts '----------Creating candidate 1/3 done----------'
+puts '----------Creating candidate 1/1 done----------'
 
 candidate_two = Candidate.new(
   first_name: 'Jean-Luc',
@@ -95,6 +99,68 @@ candidate_four.political_party = political_four
 candidate_four.user = user_admin
 candidate_four.save
 puts '----------Creating candidate 4/4 done----------'
+
+candidate_five = Candidate.new(
+  first_name: 'Valérie',
+  last_name: 'Pécresse',
+  price_per_day: '500',
+  description: "Je vais ressortir le Karcher de la cave. Cela fait dix ans qu’il y est et il est temps de l’utiliser. Il s’agit de remettre de l’ordre dans la rue.",
+  latitude: 48.880438,
+  longitude: 2.291582,
+  address: '8 rue torricelli, 75017 paris',
+  availability: true
+)
+candidate_five.political_party = political_five
+candidate_five.user = user_admin
+candidate_five.save
+
+puts '----------Creating candidate 5/5 done----------'
+
+candidate_six = Candidate.new(
+  first_name: 'Yannick',
+  last_name: 'Jadot',
+  price_per_day: '20',
+  description: "L'écologie ce n'est pas prendre une douche froide dans le noir une fois par semaine.",
+  latitude: 48.874676,
+  longitude: 2.332760,
+  address: "58 rue de la chaussée d'antin, paris",
+  availability: true
+)
+candidate_six.political_party = political_six
+candidate_six.user = user_admin
+candidate_six.save
+puts '----------Creating candidate 6/6 done----------'
+
+candidate_seven = Candidate.new(
+  first_name: 'Hélène',
+  last_name: 'Thouy',
+  price_per_day: '3',
+  description: "Les marques sont toujours plus nombreuses à abandonner la fourrure animale ! ",
+  latitude: 43.605769,
+  longitude: 1.447566,
+  address: "2 rue d’Austerlitz 31000, Toulouse",
+  availability: true
+)
+candidate_seven.political_party = political_seven
+candidate_seven.user = user_admin
+candidate_seven.save
+puts '----------Creating candidate 7/7 done----------'
+
+candidate_eight = Candidate.new(
+  first_name: 'François',
+  last_name: 'Asselineau',
+  price_per_day: '66',
+  description: "C'est un peu comme le sparadrap du capitaine Haddock dans « L'affaire Tournesol » , on ne s'en débarrasse jamais.",
+  latitude: 48.845965,
+  longitude: 2.385109,
+  address: "15 Rue Erard,
+  75012 Paris",
+  availability: true
+)
+candidate_eight.political_party = political_eight
+candidate_eight.user = user_admin
+candidate_eight.save
+puts '----------Creating candidate 8/8 done----------'
 
 puts 'Creating some bookings'
 book1 = Booking.new(
