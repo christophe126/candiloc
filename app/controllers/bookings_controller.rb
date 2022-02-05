3f57aca7d9ee4ae5767a927d695ed075f34926d7
Bastien <genin440@gmail.com>
2022-02-05 02:32:29 +0100

    added css to bookings/new

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -8,27 +8,23 @@ class BookingsController < ApplicationController
 
   def create
     @booking = Booking.new(booking_params)
-    # Récupérer la start date >> variable
-    start_date = Date.new(params['start_date'])
 
-    # Récupérer la end date >> variable
-    end_date = Date.new(params['end_date'])
+    start_date = Date.parse(params[:booking][:start_date])
 
-    # Récupérer le nombres de jours >> soustraction
+    end_date = Date.parse(params[:booking][:end_date])
+
     total_days = end_date - start_date
 
-    # Récupérer le @candidate.price_per_day
     price_per_day = @candidate.price_per_day
 
-    # Calculer le nouveau prix ppd * jours
     total_price = price_per_day * total_days
 
-    # Sauvegarder le nouveau prix dans le booking
     @booking.total_price = total_price
 
     @booking.user = current_user
-    @booking.candidate = @candidate
-    byebug
+
+    @booking.candidate_id = @candidate
+
     if @booking.save
       redirect_to candidate_path(@candidate)
     else
