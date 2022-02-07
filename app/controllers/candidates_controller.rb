ea52e9b60bbbc5bb0e06b19c2d961c3eb02f504f
medericmesle <mederic.mesle@gmail.com>
2022-02-07 20:55:03 +0100

    seed working for candidate 1

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -1,5 +1,5 @@
 class CandidatesController < ApplicationController
-    #skip_before_action :authenticate_user!, only: :index
+  # skip_before_action :authenticate_user!, only: :index
   def index
     if params[:query].present?
       @query = params[:query]
@@ -11,7 +11,7 @@ class CandidatesController < ApplicationController
     else
       @candidates = Candidate.where.not(latitude: nil, longitude: nil)
     end
-    
+
     @markers = @candidates.map do |candidate|
       {
         lat: candidate.latitude,
@@ -22,6 +22,11 @@ class CandidatesController < ApplicationController
 
   def show
     @candidate = Candidate.find(params[:id])
+    @photo_main = @candidate.photos.find_by(blob_id: 1)
+    @photo_thumb_1 = @candidate.photos.find_by(blob_id: 2)
+    @photo_thumb_2 = @candidate.photos.find_by(blob_id: 3)
+    @photo_thumb_3 = @candidate.photos.find_by(blob_id: 4)
+    @photo_thumb_4 = @candidate.photos.find_by(blob_id: 5)
   end
 
   def new
@@ -42,6 +47,7 @@ class CandidatesController < ApplicationController
   private
 
   def candidates_params
-    params.require(:candidate).permit(:first_name, :last_name, :description, :price_per_day, :political_party_id, photos: [])
+    params.require(:candidate).permit(:first_name, :last_name, :description, :price_per_day, :political_party_id,
+                                      photos: [])
   end
 end
