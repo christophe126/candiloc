9590c16c0a4369cd3d88f810ab51add1645d76e7
christophe126 <christophe126@gmail.com>
2022-02-08 18:50:01 +0100

    review encours

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,19 @@
 class ReviewsController < ApplicationController
+  def create
+    @review = Review.new(review_params)
+
+    if @review.save
+      redirect_to bookings_path
+      raise
+    else
+      render "bookings/index"
+      raise
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:comment, :rating)
+  end
 end
