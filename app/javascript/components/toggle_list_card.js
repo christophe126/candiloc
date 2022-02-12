9528902d1f74564cd645bcc6f9c5c2e37b53d373
christophe126 <christophe126@gmail.com>
2022-02-12 01:56:13 +0100

    fix color background

--- app/javascript/components/toggle_list_card.js
+++ app/javascript/components/toggle_list_card.js
@@ -2,34 +2,17 @@ const card = document.querySelector("#disp_card");
 const list = document.querySelector("#disp_list");
 const bt_switch = document.getElementById("btswitch");
 
-// toggle_list_card.addEventListener("click", (event) => {
-//   event.preventDefault();
-//   if (list.style.display = "none") {
-//     list.style.display = "flex";
-//     card.style.display = "none";
-//   } else {
-//     list.style.display = "none";
-//     card.style.display = "flex";
-//   };
-
-// });
-
-
 
 const toggle_list_card = bt_switch.addEventListener("click", (event) => {
     event.preventDefault();
     if (card.style.display === 'flex') {
       list.style.display = 'flex';
       card.style.display = 'none';
-      
-
     } else {
       list.style.display = 'none';
       card.style.display = 'flex';
     };
 });
-
-
 
 
 export { toggle_list_card };
